331a0d0579e502a9d3a0f9cb6a56a9266f9ce82a
vibhav <vibhav@almende.org>
2020-09-18 13:38:52 +0200

    removed build devel

--- multibot_ws/build/mb_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vibhav/multibot_ws/src/mb_description
-# Build directory: /home/vibhav/multibot_ws/build/mb_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- multibot_ws/build/tb3_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vibhav/multibot_ws/src/tb3_description
-# Build directory: /home/vibhav/multibot_ws/build/tb3_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
